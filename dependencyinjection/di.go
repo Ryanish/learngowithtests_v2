3340d50caf06571ebc1004bb81e2e07fb2dc6f8c
Ryanish <rybulloch@gmail.com>
2023-05-10 21:16:39 +0100

    mocking added

---
+++ dependencyinjection/di.go
@@ -0,0 +1,22 @@
+package dependencyinjection
+
+//should be run as main to rnu web services under localhost..
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+func Greet(writer io.Writer, name string) {
+	fmt.Fprintf(writer, "Hello, %s", name)
+}
+
+func MyGreeterHandler(w http.ResponseWriter, r *http.Request) {
+	Greet(w, "world")
+}
+
+func main() {
+	log.Fatal(http.ListenAndServe(":5001", http.HandlerFunc(MyGreeterHandler)))
+}
---
+++ mocking/mocking.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"time"
+)
+
+// Sleeper allows you to put delays.
+type Sleeper interface {
+	Sleep()
+}
+
+// DefaultSleeper is an implementation of Sleeper with a predefined delay.
+type DefaultSleeper struct{}
+
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep    func(time.Duration)
+}
+
+type SpyTime struct {
+	durationSlept time.Duration
+}
+
+func (s *SpyTime) Sleep(duration time.Duration) {
+	s.durationSlept = duration
+}
+
+// Sleep will pause execution for the defined Duration.
+func (d *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+
+const finalWord = "Go!"
+const countdownStart = 3
+
+// Countdown prints a countdown from 3 to out with a delay between count provided by Sleeper.
+func Countdown(out io.Writer, sleeper Sleeper) {
+
+	for i := countdownStart; i > 0; i-- {
+		fmt.Fprintln(out, i)
+		sleeper.Sleep()
+	}
+
+	fmt.Fprint(out, finalWord)
+}
+
+func (c *ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
+}
+
+func main() {
+	sleeper := &ConfigurableSleeper{1 * time.Second, time.Sleep}
+	Countdown(os.Stdout, sleeper)
+}
